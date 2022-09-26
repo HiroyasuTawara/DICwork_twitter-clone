11b424fc8e806384803f992c9a6398c08b25de57
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-09-26 11:36:15 +0900

    Add:ページリンク、投稿一覧、投稿削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = Post.all
   end
 
   def new
@@ -10,10 +12,20 @@ class PostsController < ApplicationController
     Post.create(post_params)
     redirect_to new_post_path
   end
-end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"投稿を削除しました。"
+  end
 
 private
 
   def post_params
     params.require(:post).permit(:content)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
+end
