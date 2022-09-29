699609d0845944c5f1d3046707a10dc8afb5b0ef
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-09-29 10:37:50 +0900

    Improve: rootメソッドを設定、コントローラーをリファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -10,18 +10,17 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if params[:back]
-      render :new
+    return render :new if params[:back]
+    
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました"
     else
-      if @post.save
-        redirect_to posts_path, notice: "投稿しました"
-      else
-        render :new
-      end
+      render :new
     end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def confirm
@@ -30,6 +29,7 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "投稿を編集しました"
     else
@@ -38,6 +38,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice:"投稿を削除しました。"
   end
@@ -46,10 +47,6 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
-  end
-
-  def set_post
-    @post = Post.find(params[:id])
   end
 
 end
